efde9a0219167352b6a537af9d19a9128e8c30fb
Charith Anjana <charithanjana01@gmail.com>
2023-05-06 22:49:30 +0530

    add string lengths to model classes

--- SampleAPI/Models/Customer.cs
+++ SampleAPI/Models/Customer.cs
@@ -1,15 +1,21 @@
-﻿namespace SampleAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPI.Models
 {
     public class Customer
     {
         public Guid UserId { get; set; }
 
+        [StringLength(30)]
         public string Username { get; set; }
 
+        [StringLength(20)]
         public string Email { get; set; }
 
+        [StringLength(20)]
         public string FirstName { get; set; }
 
+        [StringLength(20)]
         public string LastName { get; set; }
 
         public DateTime CreatedOn { get; set; }
