c0be563d3d3896001322ad923fed9808598011d4
Charith Anjana <charithanjana01@gmail.com>
2023-05-06 12:43:03 +0530

    Add models and install nuget package

---
+++ SampleAPI/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace SampleAPI.Models
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public int OrderStatus { get; set; }
+
+        public int OrderType { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public DateTime OrderedOn { get; set; }
+
+        public DateTime ShippedOn { get; set; }
+
+        public int IsActive { get; set; }
+
+        public Product product { get; set; }
+
+        public Customer customer { get; set; }
+    }
+}
---
+++ SampleAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleAPI.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public string SupplierId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int IsActive { get; set; }
+
+        public Supplier supplier { get; set; }
+    }
+}
